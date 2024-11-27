61f0998de3d8d608a4c06ee43503adc5e69e0511
Braja1992 <jhmbraja606@gmail.com>
2024-11-27 18:21:48 +0530

    Initial interface for calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# calculator.py
+class Calculator:
+    def addition(self, a, b):
+        pass
+    
+    def subtraction(self, a, b):
+        pass
+        
+    def division(self, a, b):
+        pass
+        
+    def multiplication(self, a, b):
+        pass
